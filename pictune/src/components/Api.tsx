7de48e355de4d2551a43454cca793cf775fad3f7
nechamiToledano <nechami3142@gmail.com>
2025-03-27 15:03:01 +0200

    axios

--- pictune/src/components/Api.tsx
+++ pictune/src/components/Api.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// Set up Axios instance
 const api = axios.create({
-  baseURL:  `${process.env.REACT_APP_API_KEY}/api`
+  baseURL: `${import.meta.env.VITE_API_KEY}/api`
 });
+
 
 // Add an interceptor to include the token in all requests
 api.interceptors.request.use(
